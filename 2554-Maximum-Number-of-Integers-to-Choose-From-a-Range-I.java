fad98adcb328cbd3dbacb66e894cb37d16561cc8
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2024-12-06 13:51:18 +0530

    [2554] [Time Beats: 96.20%] [Memory Beats: 33.18%] - LeetPush

--- 2554-Maximum-Number-of-Integers-to-Choose-From-a-Range-I.java
+++ 2554-Maximum-Number-of-Integers-to-Choose-From-a-Range-I.java
@@ -1,10 +1,13 @@
 class Solution {
     public int maxCount(int[] banned, int n, int maxSum) {
         int sum=0,ct=0;
-        Set<Integer>s=new HashSet<>();
-        for(int i:banned)s.add(i);
+        int arr[]=new int[n+1];
+        for(int i:banned){
+            if(i<=n)
+            arr[i]=1;
+        }
         for(int i=1;i<=n;i++){
-          if(!s.contains(i)){
+          if(arr[i]==0){
             if(sum+i>maxSum)return ct;
             sum+=i;
             ct++;
