909423c5cc2f5ee679d077d9536de7f10f7d5b54
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2024-12-16 06:53:30 +0530

    [3264] [Time Beats: 100.00%] [Memory Beats: 80.92%] - LeetPush

---
+++ 3264-Final-Array-State-After-K-Multiplication-Operations-I.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+       
+        for(int i=0;i<k;i++){
+            int min=nums[0],ind=0;
+          for(int j=0;j<nums.length;j++){
+            if(min>nums[j]){
+                min=nums[j];
+                ind=j;
+            }
+            
+          }
+          nums[ind]=nums[ind]*multiplier;
+            
+   
+
+        }
+   
+        return nums;
+    }
+}
