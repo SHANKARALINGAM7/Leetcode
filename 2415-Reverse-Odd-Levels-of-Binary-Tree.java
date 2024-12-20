110a41dab91d726e01f70a265d0ac1c0145de552
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2024-12-20 07:43:50 +0530

    [2415] [Time Beats: 72.88%] [Memory Beats: 61.99%] - LeetPush

--- 2415-Reverse-Odd-Levels-of-Binary-Tree.java
+++ 2415-Reverse-Odd-Levels-of-Binary-Tree.java
@@ -15,52 +15,19 @@
  */
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-          List<List<Integer>>values=new ArrayList<>();
-          Queue<TreeNode>qu=new LinkedList<>();
-          if(root==null || root.left==null)return root;
-          qu.add(root);
-          int flag=1;
-          while(qu.size()>0){
-            List<Integer>sub=new ArrayList<>();
-            int n=qu.size();
-            for(int i=0;i<n;i++){
-                TreeNode current = qu.poll();
-                if (current.left != null) {
-                    qu.add(current.left);
-                    qu.add(current.right);
-                    if(flag==1){
-                        sub.add(0,current.left.val);
-                        sub.add(0,current.right.val);
-                    }
-                }
-            }
-           if(flag==1)values.add(sub);
-           flag=1-flag;
-          }
-          int i=0;
-          qu.add(root);
-          flag=1;
-          System.out.println(values);
-          while(qu.size()>0){
-            int n=qu.size(),j=0;
-            for(int k=0;k<n;k++){
-               TreeNode current = qu.poll();
-                if (current.left != null) {
-                    qu.add(current.left);
-                    qu.add(current.right);
-                    if(flag==1){
-                      //  System.out.println(i+" "+j);
-                        current.left.val=values.get(i).get(j++);
-                       // System.out.println(i+" "+j);
-                        current.right.val=values.get(i).get(j++);
-                    }
-                }
-            }
-           if(flag==1)i++;
-           flag=1-flag;
-          
-          }
+        if(root.left==null)return root;
+          help(root.left,root.right,0);
           return root;
+    }
+    public void help(TreeNode left,TreeNode right,int level){
+        if(left==null)return;
+        if(level%2==0){
+            int val=left.val;
+            left.val=right.val;
+            right.val=val;
+        }
+        help(left.left,right.right,1+level);
+        help(left.right,right.left,1+level);
     }
    
 }
