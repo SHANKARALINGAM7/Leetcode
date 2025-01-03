439c55c9498429997f5685ded195af3572dae9ba
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-01-03 14:13:05 +0530

    Time: 3 ms (18.59%), Space: 41.5 MB (71.72%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,11 +1,21 @@
 class Solution {
+       int ans=0;
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
-        int count=0,n=citations.length,j=1;
-        for(int i=n-1;i>=0;i--){
-            if(citations[i]>=j++)count++;
-            else break;
-        }
-        return count;
+        int n=citations.length;
+        binarySearch(citations,0,n-1,n);
+        return ans;
+    }
+    public void binarySearch(int arr[],int low,int high,int n){
+      if(low<=high){
+       int mid=(low+high)/2;
+       if(n-mid<=arr[mid]){
+        ans=Math.max(ans,n-mid);
+        binarySearch(arr,low,mid-1,n);
+       }
+       else{
+        binarySearch(arr,mid+1,high,n);
+       }
+     }
     }
 }
