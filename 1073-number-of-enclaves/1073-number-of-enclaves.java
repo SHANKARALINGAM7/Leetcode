805eed97682bdb26ae77f2024e30591423add3d2
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-01-24 06:36:54 +0530

    Time: 7 ms (87.9%), Space: 56.7 MB (53.78%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.java
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -1,27 +1,43 @@
 class Solution {
+    public void dfs(int[][] grid,int i,int j){
+        if(i<0 || j<0 ||i>=grid.length || j>=grid[0].length ||grid[i][j]!=1){
+                      return;
+                }
+         grid[i][j]=2;
+         dfs(grid,i-1,j);
+          dfs(grid,i+1,j);
+           dfs(grid,i,j-1);
+           dfs(grid,i,j+1);
+           
+
+           
+
+            
+    }
     public int numEnclaves(int[][] grid) {
         int row=grid.length,col=grid[0].length;
-        Queue<int[]>q=new LinkedList<>();
+        //Queue<int[]>q=new LinkedList<>();
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if((i==0 || j==0|| i==row-1|| j==col-1)&&grid[i][j]==1){
-                    q.add(new int[]{i,j});
+                    //q.add(new int[]{i,j});
+                    dfs(grid,i,j);
                     grid[i][j]=2;
                 }
             }
         }
-        int path[][]={{-1,0},{1,0},{0,1},{0,-1}};
-        while(q.size()>0){
-            int arr[]=q.poll();
-            for(int i=0;i<4;i++){
-                int x=arr[0]+path[i][0],y=arr[1]+path[i][1];
-                if(x<0 || y<0 ||x>=row || y>=col ||grid[x][y]!=1){
-                      continue;
-                }
-                grid[x][y]=2;
-                q.add(new int[]{x,y});
-            }
-        }
+        // int path[][]={{-1,0},{1,0},{0,1},{0,-1}};
+        // while(q.size()>0){
+        //     int arr[]=q.poll();
+        //     for(int i=0;i<4;i++){
+        //         int x=arr[0]+path[i][0],y=arr[1]+path[i][1];
+        //         if(x<0 || y<0 ||x>=row || y>=col ||grid[x][y]!=1){
+        //               continue;
+        //         }
+        //         grid[x][y]=2;
+        //         q.add(new int[]{x,y});
+        //     }
+        // }
         int ct=0;
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
