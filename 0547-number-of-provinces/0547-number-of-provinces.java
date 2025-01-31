412331d8e2af5e68a24611b182d8b53574221428
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-01-31 18:38:51 +0530

    Time: 2 ms (47.25%), Space: 48.1 MB (36.98%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,22 +1,57 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-      int count=0,n=isConnected.length;
-      int visited[]=new int[n+1];
-      for(int i=1;i<=n;i++){
-         if(visited[i]==0){
-             visited[i]=-1;
-            dfs(isConnected,visited,i-1,n);
-            count++;
-         }
-      }
-      return count;
-    }
-    public void dfs(int adj[][],int[]visited,int i,int n){
-        for(int j=0;j<n;j++){
-            if(visited[j+1]==0 && adj[i][j]!=0){
-                   visited[j+1]=-1;
-                   dfs(adj,visited,j,n);
+    public int findCircleNum(int[][] arr) {
+        int n=arr.length;
+        DisJointSet ds=new DisJointSet(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(arr[i][j]==1){
+                        ds.union(i,j);
+                }
             }
         }
+        Set<Integer>s=new HashSet<>();
+        for(int i=0;i<n;i++){
+            s.add(ds.findPar(i));
+        }
+        return s.size();
+    }
+
+    public class DisJointSet{
+        int parent[];
+        int size[];
+
+        public DisJointSet(int n){
+            parent=new int[n];
+            size=new int[n];
+            for(int i=0;i<n;i++){
+                size[i]=1;
+                parent[i]=i;
+            }
+        }
+        public int findPar(int node){
+                int node2=parent[node];
+                if(node==node2)return node;
+                int u_par=findPar(node2);
+                parent[node]=u_par;
+                return u_par;
+        }
+
+        public void union(int u,int v){
+            int u_par=findPar(u);
+            int v_par=findPar(v);
+            if(u_par==v_par)return;
+            int u_size=size[u_par];
+            int v_size=size[v_par];
+
+            if(u_size<=v_size){
+                parent[u_par]=v_par;
+                size[v_par]=u_size+v_size;
+            }
+            else {
+                parent[v_par]=u_par;
+                size[u_par]=u_size+v_size;
+            }
+        }
+        
     }
 }
