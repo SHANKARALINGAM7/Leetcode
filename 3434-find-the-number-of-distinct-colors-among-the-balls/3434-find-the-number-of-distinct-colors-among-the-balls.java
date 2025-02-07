3da370bb5874472a0db04591a3fe0c7605bd1c6c
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-02-07 13:50:02 +0530

    Time: 39 ms (80.79%), Space: 81.7 MB (84.72%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
@@ -9,30 +9,19 @@ class Solution {
            if(!color.containsKey(a[1])){
             color.put(a[1],1);
             ct++;
-            if(ball.containsKey(a[0])){
-                int Ball=ball.get(a[0]);
-                  color.put(Ball,color.get(Ball)-1);
-                  if(color.get(Ball)==0){
-                    color.remove(Ball);
-                    ct--;
-                  }
-                  ball.put(a[0],a[1]);
-             }
-             else ball.put(a[0],a[1]);
            }
            else{
                 color.put(a[1],color.get(a[1])+1);
-                if(ball.containsKey(a[0])){
-                int Ball=ball.get(a[0]);
+            }
+          if(ball.containsKey(a[0])){
+                  int Ball=ball.get(a[0]);
                   color.put(Ball,color.get(Ball)-1);
                   if(color.get(Ball)==0){
                     color.remove(Ball);
                     ct--;
                   }
-                  ball.put(a[0],a[1]);
-                   }
-                  else  ball.put(a[0],a[1]);
-           }
+              }
+            ball.put(a[0],a[1]);
             ans[i++]=ct;
          }
          return ans;
