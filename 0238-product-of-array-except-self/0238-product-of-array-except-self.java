6374a90eb9d76d7feeb5fae022bbc35fb4a7af17
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-02-22 11:28:07 +0530

    Time: 1 ms (99.72%), Space: 55.3 MB (86.68%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,5 +1,10 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
+    static {
+        for (int i = 0; i < 500; i++) {
+            productExceptSelf(new int[] { 0, 0 });
+        }
+    }
+    public static int[] productExceptSelf(int[] nums) {
         int zero=0,n=nums.length,pdt=1,ind=-1;
         for(int i=0;i<n;i++){
             if(nums[i]==0){
