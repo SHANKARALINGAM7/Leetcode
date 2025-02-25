efa6b106ae73d79fbfc2cf92e720f4714d186667
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-02-25 21:56:35 +0530

    Time: 0 ms (100%), Space: 40.5 MB (91.93%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int i=0;
+        while(n!=1 &&i<25){
+            int num=0;
+            while(n>0){
+              num+=(n%10)*(n%10);
+              n/=10;
+            }
+            n=num;
+            i++;
+        }
+        return n==1;
+    }
+}
