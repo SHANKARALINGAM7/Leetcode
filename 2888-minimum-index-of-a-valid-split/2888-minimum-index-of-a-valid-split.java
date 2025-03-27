fcc9fcc48440d1f5469f9d744199d3b8d1b7c8c3
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-03-27 18:09:24 +0530

    Time: 44 ms (30.77%), Space: 69.6 MB (5.77%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -3,23 +3,18 @@ class Solution {
         int n=nums.size();
         int pre[]=new int[n];
         int suf[]=new int[n];
-        findPre(pre,n,nums);
         findSuf(suf,n,nums);
+
+        HashMap<Integer,Integer>hm=new HashMap<>();
         for(int i=0;i<n;i++){
+            hm.put(nums.get(i),hm.getOrDefault(nums.get(i),0)+1);
+            pre[i]=hm.get(nums.get(i));
             if((i+1<2*pre[i] && (n-i-1)<2*(suf[i]-1)) || (i+1<2*(pre[i]-1) &&(n-i-1)<2*suf[i])){
                  return i;
             }
         }
         return -1;
         
-    }
-
-    public void findPre(int a[],int n,List<Integer>l){
-        HashMap<Integer,Integer>hm=new HashMap<>();
-        for(int i=0;i<n;i++){
-            hm.put(l.get(i),hm.getOrDefault(l.get(i),0)+1);
-            a[i]=hm.get(l.get(i));
-        }
     }
 
     public void findSuf(int a[],int n,List<Integer>l){
