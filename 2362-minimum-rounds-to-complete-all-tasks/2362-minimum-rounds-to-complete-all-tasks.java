a241966a356db2d747075d264d07fa4b2ad4a2a2
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-04-07 01:09:17 +0530

    Time: 35 ms (28.88%), Space: 57.7 MB (54.92%) - LeetHub

---
+++ 2362-minimum-rounds-to-complete-all-tasks/2362-minimum-rounds-to-complete-all-tasks.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumRounds(int[] nums) {
+         HashMap<Integer,Integer>hm=new HashMap<>();
+           for(int i:nums){
+               hm.put(i,hm.getOrDefault(i,0)+1);
+           }
+           int count=0;
+           for(int i:hm.keySet()){
+                int freq=hm.get(i);
+                if(freq==1)return -1;
+                count+=freq/3;
+                if(freq%3!=0)count++;
+           }
+           return count;
+    }
+}
