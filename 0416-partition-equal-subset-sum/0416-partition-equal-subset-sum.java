6ef173b71b53df20b075fd14036888fa9afef73c
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-04-07 09:45:34 +0530

    Time: 103 ms (14.12%), Space: 56.2 MB (39.07%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -3,14 +3,14 @@ class Solution {
         int tot=0,n=nums.length;
         for(int i:nums)tot+=i;
         if(tot%2==1)return false;
-        int dp[][]=new int[n][tot];
+        int dp[][]=new int[n][tot/2];
         return help(nums,0,tot,0,dp)==1;
     }
 
     public int help(int nums[],int i,int tot,int cur,int dp[][]){
          
          if(i==nums.length && tot==2*cur)return 1;
-         if(cur>=tot || i>=nums.length)return -1;
+         if(cur>=tot/2 || i>=nums.length)return -1;
          if(dp[i][cur]!=0)return dp[i][cur];
          int pick=help(nums,i+1,tot,cur+nums[i],dp);
          int unpick=help(nums,i+1,tot,cur,dp);
