4042204f42be6c3872144d3da7422d5072cee1b1
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-04-17 01:13:11 +0530

    Time: 17 ms (91.25%), Space: 50.9 MB (46.42%) - LeetHub

--- 1487-cinema-seat-allocation/1487-cinema-seat-allocation.java
+++ 1487-cinema-seat-allocation/1487-cinema-seat-allocation.java
@@ -1,20 +1,20 @@
 class Solution {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
         
-         Map<Integer,Set<Integer>> hm = new HashMap<>();
+         Map<Integer,int[]> hm = new HashMap<>();
          for(int a[] : reservedSeats){
-            hm.putIfAbsent(a[0],new TreeSet<>());
-            hm.get(a[0]).add(a[1]);
+            hm.putIfAbsent(a[0],new  int[11]);
+            hm.get(a[0])[a[1]]=1;
          }
          int ans = (n-hm.size())*2;
-         for(Set<Integer> s : hm.values()){
+         for(int[] a : hm.values()){
              int prev=0,count=0;
-             if(!s.contains(2) && !s.contains(3) && !s.contains(4) && !s.contains(5)){
+             if(a[2]==0 && a[3]==0 && a[4]==0 && a[5]==0){
                  count++;
-                  if(!s.contains(6) && !s.contains(7) && !s.contains(8) && !s.contains(9))count++;
+                  if(a[6]==0 && a[7]==0 && a[8]==0 && a[9]==0)count++;
              }
-             else if(!s.contains(6) && !s.contains(7) && !s.contains(8) && !s.contains(9))count++;
-             else if(!s.contains(4) && !s.contains(5) && !s.contains(6) && !s.contains(7))count++;
+             else if(a[6]==0 && a[7]==0 && a[8]==0 && a[9]==0)count++;
+             else if(a[4]==0 && a[5]==0 && a[6]==0 && a[7]==0)count++;
              ans+=count;
          }
 
