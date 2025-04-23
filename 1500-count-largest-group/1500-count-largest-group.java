1f757b643acfa69d920a220218dd98d8a5d8d523
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-04-23 10:41:11 +0530

    Time: 5 ms (73.12%), Space: 40.4 MB (90.68%) - LeetHub

---
+++ 1500-count-largest-group/1500-count-largest-group.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countLargestGroup(int n) {
+        int freq[] = new int[37];
+        int max = 0;
+        for(int i=1;i<=n;i++){
+            int num = help(i);
+            freq[num]++;
+            max=Math.max(max,freq[num]);
+        }
+       int count = 0;
+       for(int i : freq) if(i==max)count++;
+       return count;
+    }
+
+    public int help(int n){
+        int sum = 0;
+        while(n>0){
+            sum+=n%10;
+            n/=10;
+        }
+        return sum;
+    }
+}
+
