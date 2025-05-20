18b46e6f4b83ffe0dd18d0bc00c3c239905f039a
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-05-20 11:58:38 +0530

    Time: 3 ms (79.52%), Space: 98 MB (5.4%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
@@ -1,16 +1,14 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
-        int n = nums.length;
+        int n = nums.length ,sum = 0;
         int temp[] = new int[n+1];
         for(int a[] : queries){
             temp[a[0]]++;
             temp[a[1]+1]--;
         }
-        for(int i=1;i<n;i++){
-            temp[i] += temp[i-1];
-        }
         for(int i=0;i<n;i++){
-              if(nums[i]>temp[i])return false;
+            sum += temp[i];
+            if(nums[i]>sum)return false;
         }
         return true;
     }
