4269c868b58faabdbb63f67415f71676bc5f8808
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-06-12 15:09:03 +0530

    Time: 3 ms (95.63%), Space: 56.6 MB (25%) - LeetHub

--- 3374-count-alternating-subarrays/3374-count-alternating-subarrays.java
+++ 3374-count-alternating-subarrays/3374-count-alternating-subarrays.java
@@ -1,25 +1,18 @@
 class Solution {
-    long factorial[] = new long[100001];  
-    public void fact(int n){
-       factorial[1] = 1;
-       for(int i=2;i<=n;i++){
-             factorial[i] = factorial[i-1] + i;
-       }
-    }
-
+   
     public long countAlternatingSubarrays(int[] nums) {
         long ans = 0;
         
         int n = nums.length , left = 0;
-        fact(n);
         for(int i=1;i<n;i++){
              if(nums [i] == nums[i-1]){
-                ans += factorial[i-left];
+                long k = i-left;
+                ans += (k*(k+1))/2;
                 left = i;
              }
         }
-        ans += factorial[n-left];
-      
+         long k = n-left;
+         ans += (k*(k+1))/2;   
         return ans;
     }
 }
