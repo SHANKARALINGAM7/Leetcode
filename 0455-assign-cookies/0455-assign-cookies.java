c883f8a1b9a1bae2959da6ed0f13061b2071f720
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-06-16 22:03:19 +0530

    Time: 11 ms (39.31%), Space: 47.5 MB (99.74%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -4,14 +4,16 @@ class Solution {
         int n = g.length;
         int m = s.length;
         int  i = 0 , j = 0 , count =0;
-         Thread t1 = new Thread(() -> Arrays.sort(g));
-        Thread t2 = new Thread(() -> Arrays.sort(s));
-        t1.start();
-        t2.start();
-        try{
-            t1.join();
-            t2.join();
-        }catch(Exception e){}
+        Arrays.sort(g);
+        Arrays.sort(s);
+        //  Thread t1 = new Thread(() -> Arrays.sort(g));
+        // Thread t2 = new Thread(() -> Arrays.sort(s));
+        // t1.start();
+        // t2.start();
+        // try{
+        //     t1.join();
+        //     t2.join();
+        // }catch(Exception e){}
 
         while(i<n && j<m){
              if(s[j] >= g[i]){
