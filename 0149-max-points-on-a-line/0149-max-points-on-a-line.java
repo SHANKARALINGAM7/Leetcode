52fe38090a2a9472c1584b0f24abb993b5ee954d
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-06-16 14:49:43 +0530

    Time: 143 ms (5.04%), Space: 45 MB (38.44%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -0,0 +1,28 @@
+class Solution {
+    public double helper(int x[] ,int y[]){
+         if (y[0] - x[0] == 0) { 
+            return Double.POSITIVE_INFINITY;
+         }
+          double slope = (double)(y[1]-x[1])/(y[0]-x[0]);
+
+          return slope;
+    }
+    public int maxPoints(int[][] points) {
+        int ans = 0;
+        int n = points.length;
+
+        for(int i=0;i<n;i++){
+            HashMap<Double,Integer> map = new HashMap<>();
+            int max = 0;
+             for(int j=0;j<n;j++){
+                if(i==j)continue;
+                   double find =  helper(points[i],points[j]);
+                   map.put(find,map.getOrDefault(find,0)+1);
+                   max = Math.max(max,map.get(find));
+             }
+             ans = Math.max(ans,max+1);
+        }
+
+        return ans;
+    }
+}
