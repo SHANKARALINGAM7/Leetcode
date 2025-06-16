f233b73a1be8d1598fb182f5b2aca2f91aa035ae
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-06-16 22:09:00 +0530

    Time: 6 ms (100%), Space: 64.4 MB (34.77%) - LeetHub

---
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int matchPlayersAndTrainers(int[] g, int[] s) {
+         int n = g.length;
+        int m = s.length;
+        int  i = 0 , j = 0 , count =0;
+        Thread t1 = new Thread(() -> Arrays.sort(g));
+        Thread t2 = new Thread(() -> Arrays.sort(s));
+        t1.start();
+        t2.start();
+        try{
+            t1.join();
+            t2.join();
+        }catch(Exception e){}
+
+        while(i<n && j<m){
+             if(s[j] >= g[i]){
+                i++;
+                j++;
+                count++;
+             }
+             else j++;
+        }
+
+        return count;
+    }
+}
