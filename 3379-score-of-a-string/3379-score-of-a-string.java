86cb5a72989e311e2a24c68c47b10f44500affeb
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-07-06 14:32:59 +0530

    Time: 1 ms (99.76%), Space: 42.1 MB (64.03%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.java
+++ 3379-score-of-a-string/3379-score-of-a-string.java
@@ -2,8 +2,9 @@ class Solution {
     public int scoreOfString(String s) {
          int n = s.length();
          int ans = 0;
+         char ch[] = s.toCharArray();
          for(int i=1;i<n;i++){
-             ans = ans + Math.abs(s.charAt(i)-s.charAt(i-1));
+             ans += Math.abs(ch[i]-ch[i-1]);
          }
          return ans;
     }
