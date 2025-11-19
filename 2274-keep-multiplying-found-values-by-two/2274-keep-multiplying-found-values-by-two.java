f5bc014dae73e5adb2be23d840ff1f964c89517f
SHANKARALINGAM S <165680168+SHANKARALINGAM7@users.noreply.github.com>
2025-11-19 17:34:28 +0530

    Time: 1 ms (93.95%), Space: 46 MB (17.14%) - LeetHub

---
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+         int freq[] = new int[1001];
+         for(int i : nums){
+            freq[i]++;
+         }
+
+         while(original <= 1000 && freq[original] != 0){
+              original *= 2;
+         }
+
+         return original;
+    }
+}
